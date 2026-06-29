32cfe7ca3c23feda00e9c90d82c957af3333b52f
ny-noe-e <noe.eberli@noseryoung.ch>
2026-06-29 09:47:48 +0200

    basic systeminfos implementiert

--- systemleistung.sh
+++ systemleistung.sh
@@ -35,14 +35,56 @@ check_arguments() {
     fi
 }
 
+
+check_commands() {
+    REQUIRED_COMMANDS=("date" "uname" "uptime" "awk" "grep" "sed" "cat")
+
+    for CMD in "${REQUIRED_COMMANDS[@]}"; do
+        if ! command -v "$CMD" >/dev/null 2>&1; then
+            printf "Fehler: Der Befehl '%s' wurde nicht gefunden.\n" "$CMD"
+            exit 1
+        fi
+    done
+}
+
+
+get_system_time() {
+    date '+%Y-%m-%d %H:%M:%S'
+}
+
+
+get_system_uptime() {
+    uptime -p 2>/dev/null | sed 's/up //'
+}
+
+
+get_hostname_value() {
+    if [ -f /proc/sys/kernel/hostname ]; then
+        cat /proc/sys/kernel/hostname
+    else
+        uname -n
+    fi
+}
+
+
+get_os_name() {
+    if [ -f /etc/os-release ]; then
+        grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2 | tr -d '"'
+    else
+        uname -s
+    fi
+}
+
 main() {
     WRITE_FILE=false
 
     check_arguments "$@"
+    check_commands
 
-    printf "%s\n" "$SCRIPT_NAME"
-    printf "Version: %s\n" "$SCRIPT_VERSION"
-    printf "Dateiausgabe aktiv: %s\n" "$WRITE_FILE"
+    printf "Systemzeit: %s\n" "$(get_system_time)"
+    printf "Systemlaufzeit: %s\n" "$(get_system_uptime)"
+    printf "Hostname: %s\n" "$(get_hostname_value)"
+    printf "Betriebssystem: %s\n" "$(get_os_name)"
 }
 
 main "$@"
